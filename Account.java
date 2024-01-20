3d6f9510c3a00983f7049599ae67222a79cd1ce2
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-01-20 15:43:48 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,22 @@
+class Account{
+	String name;
+	int age;
+	String gender;
+	ArrayList<TaskSet> list = new ArrayList<TaskSet>();		
+
+	public Account(String name, int age, String gender){
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+	
+	public toString(){
+		return "User Details\nName: "+name+"\nAge: "+age+"\nGender: "+gender;
+	}
+	public void addTaskSet(TaskSet ts){
+		list.add(ts);
+	}
+	public void removeTaskSet(TaskSet ts){
+		list.remove(ts);
+	}
+}
